aec1cfd42cba83e460655ce044cdf98ac7573d53
ellerykalindaa <240030341@stikom-bali.ac.id>
2026-01-22 00:36:27 +0800

    Update category_router.py

--- Blog_Pribadi/app/routers/category_router.py
+++ Blog_Pribadi/app/routers/category_router.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from app.database.db import get_db
 from app.database.models import Category
 from app.schema.category_schema import CategoryCreate
+from app.core.security import get_current_user
 
 router = APIRouter(
     prefix="/categories",
@@ -12,11 +13,20 @@ router = APIRouter(
 
 @router.post("/")
 def create_category(
-    data: CategoryCreate,
-    db: Session = Depends(get_db)
+    category: CategoryCreate,
+    db: Session = Depends(get_db),
+    user=Depends(get_current_user)
 ):
-    category = Category(name=data.name)  # ✅ BENAR
-    db.add(category)
+    existing = db.query(Category).filter(Category.name == category.name).first()
+    if existing:
+        raise HTTPException(400, "Category already exists")
+
+    new_category = Category(name=category.name)
+    db.add(new_category)
     db.commit()
-    db.refresh(category)
-    return category
+    db.refresh(new_category)
+    return new_category
+
+@router.get("/", response_model=list[CategoryResponse])
+def get_categories(db: Session = Depends(get_db)):
+    return db.query(Category).all()
